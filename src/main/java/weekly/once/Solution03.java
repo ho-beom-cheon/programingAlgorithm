80cdc8745ab4832485368b13203e0d57d19ff0e0
hobeomcheon <cjs414ghqja@gmail.com>
2021-07-16 23:57:53 +0900

    프로그래머스 - 가장 큰 수 문제 유형 : 정렬

---
+++ src/main/java/weekly/once/Solution03.java
@@ -0,0 +1,45 @@
+package weekly.once;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/** Solution02 : 가장 큰 수
+ *  문제 유형 : 정렬
+ */
+public class Solution03 {
+    public static String solution03(int[] numbers) {
+
+        String[] result = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            result[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(result, new Comparator<String>() {
+
+            public int compare(String o1, String o2) {
+
+                return ((o2 + o1).compareTo(o1 + o2));
+            }
+        });
+
+        if(result[0].equals("0")) {
+            return "0";
+        }
+
+        String answer = "";
+        for (String a : result) {
+            answer += a;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {6,10,2};
+        int[] numbers2 = {3, 30, 34, 5, 9};
+
+        solution03(numbers);
+        System.out.println(solution03(numbers));
+        solution03(numbers2);
+        System.out.println(solution03(numbers2));
+    }
+}
