3a423260f93e0cdcee91ada35e440eb4761e7800
hobeomcheon <cjs414ghqja@gmail.com>
2021-08-01 23:11:14 +0900

    /** Solution05 : 소수찾기  *  문제 유형 : 알고리즘  *  출처 : 프로그래머스 - 연습문제  *  날짜 : 2021-08-01  */

--- src/main/java/weekly/week3/Solution05.java
+++ src/main/java/weekly/week3/Solution05.java
@@ -1,39 +1,29 @@
 package weekly.week3;
 
-/** Solution03 : 2016년
- *  문제 유형 : 연산
+/** Solution05 : 소수찾기
+ *  문제 유형 : 알고리즘
  *  출처 : 프로그래머스 - 연습문제
- *  날짜 : 2021-07-30
+ *  날짜 : 2021-08-01
  */
 public class Solution05 {
-    public static String solution(int a, int b) {
-        String answer = "";
-
-        String[] day = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
-        String[] year = new String[366];
-        int[] month = {31,29,31,30,31,30,31,31,30,31,30,31};
-        int want_day = 0;
-
-        for(int i=0; i<year.length; i++) {
-            year[i] =  day[(i+5)%7];
+    public static int solution(int n) {
+        int answer = 0;
+        for(int i = 2; i <= n; i++){
+            boolean chk = true;
+            for(int j = 2; j*j <= i; j++){
+                if(i % j == 0){
+                    chk = false; break;
+                }
+            }
+            if(chk){
+                answer++;
+            }
         }
-
-        for(int i=0; i<a-1; i++) {
-            want_day += month[i];
-        }
-
-        want_day += b-1;
-
-        answer = year[want_day];
-
         System.out.println("답 : " + answer);
-
         return answer;
     }
-
     public static void main(String[] args) {
-        int val1 = 5;
-        int val2 = 24;
-        solution(val1, val2);
+        int n = 5;
+        solution(n);
     }
 }
