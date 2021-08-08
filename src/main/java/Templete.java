b934a38df8436b6bb6a38112ce4a1bcb063aed6c
hobeomcheon <cjs414ghqja@gmail.com>
2021-08-08 23:33:57 +0900

    /** Solution05 : 제일 작은 수 제거하기  *  문제 유형 : 배열  *  출처 : 프로그래머스 - 연습문제  *  날짜 : 2021-08-08  */

---
+++ src/main/java/Templete.java
@@ -0,0 +1,21 @@
+/** Solution0 :
+ *  문제 유형 :
+ *  출처 : 프로그래머스
+ *  날짜 : 2021-08-07
+ */
+public class Templete {
+    public static int[] solution(int[] arr) {
+        int[] answer = {};
+
+        System.out.println("답 : " + answer);
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        int[] val1 = {4, 3, 2, 1};
+
+        solution(val1);
+    }
+}
