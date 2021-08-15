53be81755f681034de50e7a973a41a080fd1d8f2
hobeomcheon <cjs414ghqja@gmail.com>
2021-08-15 23:57:48 +0900

    /** Solution04 : 평균구하기  *  문제 유형 : 연산  *  출처 : 프로그래머스 - 연습문제  *  날짜 : 2021-08-15  */

---
+++ src/main/java/August/week2/Solution04.java
@@ -0,0 +1,27 @@
+package August.week2;
+
+/** Solution04 : 평균구하기
+ *  문제 유형 : 연산
+ *  출처 : 프로그래머스 - 연습문제
+ *  날짜 : 2021-08-15
+ */
+public class Solution04 {
+    public static double solution(int[] arr) {
+        double answer = 0;
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        answer = sum / arr.length;
+
+        System.out.println("답 : " + answer);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] var = {1,2,3,4};
+
+        solution(var);
+    }
+}
