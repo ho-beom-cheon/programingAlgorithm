1cf113ddf1544a61bd76a1315ef08e9490d35cde
hobeomcheon <cjs414ghqja@gmail.com>
2021-09-08 23:39:20 +0900

    /** Solution01 : x만큼 간격이 있는 n개의 숫자  *  문제 유형 : 알고리즘  *  출처 : 프로그래머스 - 연습문제  *  날짜 : 2021-09-08  */

---
+++ src/main/java/September/week2/Solution01.java
@@ -0,0 +1,27 @@
+package September.week2;
+
+/** Solution01 : x만큼 간격이 있는 n개의 숫자
+ *  문제 유형 : 알고리즘
+ *  출처 : 프로그래머스 - 연습문제
+ *  날짜 : 2021-09-08
+ */
+public class Solution01 {
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        int val = x;
+
+        for(int i =0; i<n; i++) {
+            answer[i] = ((long)x * (i+1));
+            System.out.println("답 : " + answer[i]);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int var = 2;
+        int var2 = 5;
+        solution(var, var2);
+    }
+}
+
