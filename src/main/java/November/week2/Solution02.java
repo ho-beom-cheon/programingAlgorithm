fe28a8e851dad6f8a67c93cf95cbede2f9ba2ad2
hobeomcheon <cjs414ghqja@gmail.com>
2021-11-13 12:32:38 +0900

    /** Solution02 : Isomorphic Strings  *  문제 유형 : Map  *  출처 : LeetCode  *  날짜 : 2021-11-13  */

--- src/main/java/November/week2/Solution02.java
+++ src/main/java/November/week2/Solution02.java
@@ -1,37 +1,40 @@
 package November.week2;
 
-/** Solution02 : Longest Common Prefix
- *  문제 유형 : 알고리즘
+import java.util.HashMap;
+import java.util.Map;
+
+/** Solution02 : Isomorphic Strings
+ *  문제 유형 : Map
  *  출처 : LeetCode
- *  날짜 : 2021-11-07
+ *  날짜 : 2021-11-13
  */
 public class Solution02 {
+    public static boolean isIsomorphic(String s, String t) {
+        Map<Character,Character> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char s_chars = s.charAt(i);
+            char t_chars = t.charAt(i);
 
-    public static String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) return "";
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < strs[0].length(); i++) {
-            char c = strs[0].charAt(i);
-
-            for (String str : strs) {
-                if (str.length() <= i || c != str.charAt(i)) {
-                    return sb.toString();
+            if(!map.containsKey(s_chars)){
+                if(map.containsValue(t_chars)){
+                    return false;
                 }
+                map.put(s_chars,t_chars);
             }
-
-            sb.append(c);
+            else if(map.get(s_chars) != t_chars){
+                return false;
+            }
         }
-
-        return sb.toString();
+        return true;
     }
 
     public static void main(String[] args) {
-        String[] val1 = {"flower","flow","flight"};
+        String s = "egg";
+        String t = "add";
 
-        longestCommonPrefix(val1);
+        isIsomorphic(s,t);
+        System.out.println(isIsomorphic(s,t));
 
-        System.out.println("답 : " + longestCommonPrefix(val1));
     }
 }
+
